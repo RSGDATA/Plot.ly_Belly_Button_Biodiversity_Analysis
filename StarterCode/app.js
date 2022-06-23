29b9112edcb521e47994861a05122aa9e5e8db08
Robert Gonzalez <robertsgonzalez92@yahoo.com>
2022-06-23 12:30:44 -0500

    chart progress

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,43 +1,61 @@
  
-//     //Initialize the dashboard
-// function BuildCharts(selected) {
-//     console.log(selected)
+    //Initialize the dashboard
+function buildCharts(selected) {
+    //console.log(selected)
 
-//     d3.json("samples.json").then((data) => {
+    d3.json("samples.json").then((data) => {
 
-//     console.log(data)
-//     // You need to filter by selected
+    //console.log(data)
+    // You need to filter by selected
     
+    let sample = data.samples.filter(values => values.otu_id == selected);
+        console.log(sample)
     
 //     })
 
-
+    var chart = [
+        {
+          x: sample,
+          y: data,
+          type: 'bar'
+        }
+      ];
+      
+      Plotly.newPlot('bar', chart);
+    })
 
 // }
+
+
+}
 
 
  
     
 
-// d3.json("samples.json").then((data) => {
+d3.json("samples.json").then((data) => {
 
-//     console.log(data.names)
+    //console.log(data.names)
 
-//     let dropdown = d3.select("#selDataset")
+    let dropdown = d3.select("#selDataset")
 
-//     data.names.forEach((id) => {
+    data.names.forEach((id) => {
 
-//         console.log(id)
+       // console.log(id)
+
+        dropdown.append('option').text(id).property("value", id)
 
 //         dropdown.append('option').text(id).property("value", id)
 
+    })
 
-//     })
+    buildCharts(data.names[0])
+ })
 
-//     BuildCharts(data.names)
-//  })
+function optionChanged(selected) {
 
-// function optionChanged(selected) {
+    buildCharts(selected)
+}
 
 //     BuildCharts(selected)
 // }
@@ -45,7 +63,7 @@
 //}
     
     function optionChanged(newSample) {
-        console.log(newSample)
+        //console.log(newSample)
         // fetch new data each time a new sample is selected
         buildMetadata(newSample);
         buildCharts(newSample);
@@ -80,86 +98,89 @@ function buildMetadata(sample){
   });
 }
 
-// 1. Create the buildCharts function.
-function buildCharts(sample) {
+// // 1. Create the buildCharts function.
+// function buildCharts(sample) {
 
-    console.log(sample)
+//     console.log(sample)
 
-    // 2. Use  d3.json to load and retrieve the samples.json file
-    d3.json("samples.json").then((sample) => {
+//     // 2. Use  d3.json to load and retrieve the samples.json file
+//     d3.json("samples.json").then((sample) => {
 
-        //console.log(data.samples.otu_ids)
+//         let barChart = d3.select("#bar")
+//         //console.log(data.samples.otu_ids)
 
-        sample.samples.forEach((otu_ids) => { 
+//         sample.samples.forEach((samples) => { 
 
-            console.log(otu_ids)
+//             console.log(samples.samples)
+
+//            // barChart.append('bar').text(sample_values).property("value", sample_values)
     
-    });
+//     });
 
 
-});
+// });
 
 
-        // 3. Create a variable that holds the samples array.
+//         // 3. Create a variable that holds the samples array.
         
-        // 4. Create a variable that filters the samples for the object with the desired sample number.
+//         // 4. Create a variable that filters the samples for the object with the desired sample number.
 
-        // 5. Create a variable that holds the first sample in the array
+//         // 5. Create a variable that holds the first sample in the array
 
-        // 6 Create variables that hold the otu_ids, otu_labels, and sample_values.
-
-
-        // 7. Create the yticks for the bar chart.
-        // Hint: Get the top 1- otu_ids and map them in descending order
-        // so the otu_ids with the most bacteria are last.
-
-        //let yticks =
-
-        // 8. Create the trace for thebar chart.
-        //let barData = [
+//         // 6 Create variables that hold the otu_ids, otu_labels, and sample_values.
 
 
-        //];
-        // 9. Create the trace for the bar chart.
-        //let barLayout = {
+//         // 7. Create the yticks for the bar chart.
+//         // Hint: Get the top 1- otu_ids and map them in descending order
+//         // so the otu_ids with the most bacteria are last.
 
-        //}
-        // 10. Use Plotly to plot the data with the layout
+//         //let yticks =
 
-    //});
-}
+//         // 8. Create the trace for thebar chart.
+//         //let barData = [
 
-function init() {
 
-    // Grab a reference to dropdown select element 
-     let selector = d3.select("#selDataset");
+//         //];
+//         // 9. Create the trace for the bar chart.
+//         //let barLayout = {
+
+//         //}
+//         // 10. Use Plotly to plot the data with the layout
+
+//     //});
+// }
+
+// function init() {
+
+//     // Grab a reference to dropdown select element 
+//      let selector = d3.select("#selDataset");
     
-    // Use the list of sample names to populate the select options
+//     // Use the list of sample names to populate the select options
  
-     d3.json("samples.json").then((data) => {
-         let sampleNames = data.names;
-        console.log(data)
+//      d3.json("samples.json").then((data) => {
+//          let sampleNames = data.names;
+//         console.log(data)
      
      
  
-     sampleNames.forEach((sample) => {
-         selector
-         .append ("option")
-         .text (sample)
-         .property ("value", sample);
+//      sampleNames.forEach((sample) => {
+//          selector
+//          .append ("option")
+//          .text (sample)
+//          .property ("value", sample);
  
  
-         });
+//          });
  
-         // Ise the first sample from the list to build the initial plots
-         let firstSample = sampleNames[0]; 
-         buildCharts(firstSample);
-         buildMetadata(firstSample);
+//          // Ise the first sample from the list to build the initial plots
+//          let firstSample = sampleNames[0]; 
+//          buildCharts(firstSample);
+//          buildMetadata(firstSample);
  
-     });
+//      });
  
-    }
-init();
+//     }
+// init();
 
 
 
